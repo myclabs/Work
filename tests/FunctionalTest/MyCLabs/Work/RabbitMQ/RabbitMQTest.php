975fd74a91c4ca4a518198a79b53f2bb37e0eb8b
Matthieu Napoli <matthieu@mnapoli.fr>
2013-09-19 12:02:22 +0200

    Improved tests

--- tests/FunctionalTest/MyCLabs/Work/RabbitMQ/RabbitMQTest.php
+++ tests/FunctionalTest/MyCLabs/Work/RabbitMQ/RabbitMQTest.php
@@ -5,6 +5,7 @@ namespace UnitTest\MyCLabs\Work\Dispatcher;
 use MyCLabs\Work\Dispatcher\RabbitMQWorkDispatcher;
 use MyCLabs\Work\Task\Task;
 use MyCLabs\Work\Worker\RabbitMQWorker;
+use PhpAmqpLib\Channel\AMQPChannel;
 use PhpAmqpLib\Connection\AMQPConnection;
 use PhpAmqpLib\Exception\AMQPRuntimeException;
 use PHPUnit_Framework_TestCase;
@@ -16,37 +17,96 @@ class RabbitMQTest extends PHPUnit_Framework_TestCase
 {
     const QUEUE_NAME = 'myclabs_work_test';
 
-    public function testRunBackground()
+    /**
+     * @var AMQPConnection
+     */
+    private $connection;
+
+    /**
+     * @var AMQPChannel
+     */
+    private $channel;
+
+    public function setUp()
     {
         try {
-            $connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
+            $this->connection = new AMQPConnection('localhost', 5672, 'guest', 'guest');
         } catch (AMQPRuntimeException $e) {
             // RabbitMQ not installed, mark test skipped
             $this->markTestSkipped('RabbitMQ is not installed or was not found');
             return;
         }
+        $this->channel = $this->connection->channel();
+        $this->channel->queue_declare(self::QUEUE_NAME, false, false, false, false);
+    }
 
-        $channel = $connection->channel();
+    public function tearDown()
+    {
+        $this->channel->close();
+        $this->connection->close();
+    }
 
-        $channel->queue_declare(self::QUEUE_NAME, false, false, false, false);
-
-        $workDispatcher = new RabbitMQWorkDispatcher($channel, self::QUEUE_NAME);
+    public function testRunBackground()
+    {
+        $workDispatcher = new RabbitMQWorkDispatcher($this->channel, self::QUEUE_NAME);
 
         // Pile up a task to execute
         $task = new FakeTask();
         $workDispatcher->runBackground($task);
 
         // Run the worker to execute the task
+
+        $worker = new RabbitMQWorker($this->channel, self::QUEUE_NAME);
+
+        // Check that event methods are called
+        $listener = $this->getMock('MyCLabs\Work\Worker\EventListener');
+        $listener->expects($this->once())
+            ->method('beforeTaskExecution');
+        $listener->expects($this->once())
+            ->method('onTaskSuccess');
+        $worker->addEventListener($listener);
+
+        // Fake task executor
         $taskExecutor = $this->getMockForAbstractClass('MyCLabs\Work\TaskExecutor\TaskExecutor');
         $taskExecutor->expects($this->once())
             ->method('execute')
             ->with($task);
-        $worker = new RabbitMQWorker($channel, self::QUEUE_NAME);
         $worker->registerTaskExecutor(get_class($task), $taskExecutor);
-        $worker->work(1);
 
-        $channel->close();
-        $connection->close();
+        // Work
+        $worker->work(1);
+    }
+
+    public function testRunBackgroundWithException()
+    {
+        $workDispatcher = new RabbitMQWorkDispatcher($this->channel, self::QUEUE_NAME);
+
+        // Pile up a task to execute
+        $task = new FakeTask();
+        $workDispatcher->runBackground($task);
+
+        // Run the worker to execute the task
+
+        $worker = new RabbitMQWorker($this->channel, self::QUEUE_NAME);
+
+        // Check that event methods are called
+        $listener = $this->getMock('MyCLabs\Work\Worker\EventListener');
+        $listener->expects($this->once())
+            ->method('beforeTaskExecution');
+        $listener->expects($this->once())
+            ->method('onTaskException');
+        $worker->addEventListener($listener);
+
+        // Fake task executor
+        $taskExecutor = $this->getMockForAbstractClass('MyCLabs\Work\TaskExecutor\TaskExecutor');
+        $taskExecutor->expects($this->once())
+            ->method('execute')
+            ->with($task)
+            ->will($this->throwException(new \Exception()));
+        $worker->registerTaskExecutor(get_class($task), $taskExecutor);
+
+        // Work
+        $worker->work(1);
     }
 }
 
